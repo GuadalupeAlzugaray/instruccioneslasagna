e24be719a1896abc67b16b9f55dae5e3fb2f61f7
GuadalupeAlzugaray <110639935+GuadalupeAlzugaray@users.noreply.github.com>
2022-08-06 11:40:58 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+
+
+var instrucioneslasagna = [
+"cortar cebolla"
+"cocinar la cebolla en una olla"
+"una vez que esta cocinada la carne, agregar salsa de tomate"
+"pasar por agua caliente la masa"
+"agarrar una fuente y poner una capa de masa"
+"agregar una capa de jamon"
+"agregar una capa de masa"
+"agregar queso cremoso"
+"agregar una capa de masa"
+"agregar el tuco anteriormente hecho"
+"agregar queso rayado para gratinar"
+"ponerla al horno por una hora"
+"degustar"
+]
+for (var i = 0; i < instrucioneslasagna.length; i++){
+  console.log("paso:" + [i + 1],instrucioneslasagna[i])
+}
